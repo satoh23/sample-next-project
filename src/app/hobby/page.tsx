24e0dbbc9d37ef764d3253eee9a331461bfe5385
Tetsuya Satoh <satohT@example.com>
2024-08-31 16:21:20 +0900

    趣味ページを追加

--- src/app/hobby/page.tsx
+++ src/app/hobby/page.tsx
@@ -1,10 +1,26 @@
 import Link from 'next/link';
+import { YouTubeEmbed } from "@next/third-parties/google";
 
-export default function Hoby() {
+const Hobby = () => {
   return (
-    <div className="flex flex-col min-h-screen item-center justify-center">
-      <div className="">My Hoby</div>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-yellow-100 gap-5">
+      <div className="text-5xl underline underline-offset-[12px]">My Hobby</div>
+      <div className="flex gap-5">
+        <div className="w-52 rouded overflow-hidden shadow-lg bg-yellow-300">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">音楽</div>
+            <div className="text-gray-700 text-base">
+              <li>
+                SKRYU
+                <YouTubeEmbed videoid="S42d2MkQyIk"/>
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
       <Link href="/">Homeへ</Link>
     </div>
   );
 }
+
+export default Hobby;
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
 
-export default function Work() {
+const Work = () => {
   return (
-    <div className="flex flex-col min-h-screen item-center justify-center">
-      <div className="">My Work</div>
+    <div className="flex flex-col min-h-screen item-center pt-10 bg-yellow-50 gap-5">
+      <div className="text-5xl underline underline-ofset-[12px]">My Work</div>
+      <div className="w-1/2 flex items-center justify-between border-2 border-solid border-yellow-300 p-3 rouded-lg">
+        <div className="flex text-xl">技術スタック</div>
+        <div className="text-xl gap-5">
+          <div className="text-xl">●Java</div>
+          <p>研修でJavaを学習し、そのまま現場へ</p>
+          <p>開発工程が終わりかけの時期の参画だったので、ほとんど開発できず...</p>
+        </div>
+        <div className="text-xl gap-5">
+          <div className="text-xl">●React</div>
+          <p>勉強中！</p>
+        </div>
+      </div>
       <Link href="/">Homeへ</Link>
     </div>
   );
 }
+
+export default Work;
